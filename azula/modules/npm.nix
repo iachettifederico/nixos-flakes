c02289bee2103f3ffe3c31d81351990f8aa6f496
Federico M. Iachetti <iachetti.federico@gmail.com>
2026-02-12 11:14:09 -0300

    Remove warnings

--- azula/modules/npm.nix
+++ azula/modules/npm.nix
@@ -37,17 +37,17 @@
       expat
       libdrm
 
-      xorg.libX11
-      xorg.libXcomposite
-      xorg.libXcursor
-      xorg.libXdamage
-      xorg.libXext
-      xorg.libXfixes
-      xorg.libXi
-      xorg.libXrandr
-      xorg.libXrender
-      xorg.libXtst
-      xorg.libxcb
+      libX11
+      libXcomposite
+      libXcursor
+      libXdamage
+      libXext
+      libXfixes
+      libXi
+      libXrandr
+      libXrender
+      libXtst
+      libxcb
 
       alsa-lib
       at-spi2-atk
